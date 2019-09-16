01ca9e0bd401dc11712622d1e349bab07786f368
Konrad C <conrisc@gmail.com>
2019-09-16 12:49:36 +0200

    Add jest - test framework

--- src/exampleScript.js
+++ src/exampleScript.js
@@ -1,3 +1,11 @@
 const messege = 'This example script has been loaded';
 
 console.log(messege);
+
+function fun() {
+    return true;
+}
+
+export {
+    fun
+};
---
+++ src/exampleScript.test.js
@@ -0,0 +1,6 @@
+import { fun } from './exampleScript';
+
+test('should return true', () => {
+    const result = fun();
+    expect(result).toBe(true);
+});
