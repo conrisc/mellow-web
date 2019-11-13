e61c48baff4c3708eeb60aab1386a893d2aca2b6
Konrad C <conrisc@gmail.com>
2019-11-13 10:29:10 +0100

    Adds button to search video in youtube

--- src/components/TrackList.jsx
+++ src/components/TrackList.jsx
@@ -30,6 +30,13 @@ export function TrackList(props) {
                             </div>
                             <div className="col s3 right right-text">
                                 <a
+                                href={"https://www.youtube.com/results?search_query="+songItem.title}
+                                className="btn btn-small"
+                                target="_blank" rel="noopener noreferrer"
+                                title="Search song in youtube">
+                                    <i className="fas fa-link"></i>
+                                </a>
+                                <a
                                 href={songItem.url}
                                 className={"btn btn-small" + (songItem.url ? '' : ' disabled')}
                                 target="_blank" rel="noopener noreferrer"
@@ -37,16 +44,16 @@ export function TrackList(props) {
                                     <i className="fas fa-link"></i>
                                 </a>
                                 <button
+                                className="btn btn-small"
+                                onClick={() => props.findSong(songItem.title)}
+                                title="Find this song using youtube">
+                                    <i className="fab fa-youtube"></i>
+                                </button>
+                                <button
                                 className={"btn btn-small" + (videoId ? '' : ' disabled')}
                                 onClick={() => props.loadVideo(videoId)}
                                 title="Play song on other devices">
                                     <i className="fas fa-play"></i>
-                                </button>
-                                <button
-                                className="btn btn-small"
-                                onClick={() => props.findSong(songItem.title)}
-                                title="Find this song on youtube">
-                                    <i className="fab fa-youtube"></i>
                                 </button>
                             </div>
                         </li>;
