2c8bbbf2768e7a7b2a7ddba94c6fcfe9adcfc7ca
Konrad Cielecki <conrisc@gmail.com>
2021-10-25 23:53:36 +0200

    Switches to webpackDevServer, simplifies app serve

--- server.js
+++ server.js
@@ -1,40 +1,14 @@
 const express = require('express');
-const webpack = require('webpack');
-const webpackDevMiddleware = require('webpack-dev-middleware');
-const webpackHotMiddleWare = require('webpack-hot-middleware');
 const path = require('path');
 
 const port = process.env.PORT || 8080;
 
-const argsArr = process.argv.slice(2);
-const args = {}
-for (let arg of argsArr) {
-    argSplit = arg.split('=');
-    args[argSplit[0].toLowerCase()] = argSplit[1].toLowerCase();
-}
-
-const isInProductionMode = args['mode'] === 'production';
-const config = isInProductionMode ? require('./webpack.prod') : require('./webpack.dev');
-const compiler = webpack(config);
-
 const app = express();
 
-app.use(webpackDevMiddleware(compiler, {
-    publicPath: config.output.publicPath
-}));
-
-if (!isInProductionMode) {
-    app.use(webpackHotMiddleWare(compiler));
-    app.get('/ping', function (req, res) {
-        return res.send('pong');
-    });
-}
-
-app.use(express.static(__dirname));
 app.use(express.static(path.join(__dirname, 'dist')));
 
 app.get('/*', function (req, res) {
-    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+    res.sendFile(path.join(__dirname, 'dist', 'index.html'));
 });
 
 app.listen(port, () => console.log(`Server is listening on ${port}`));
--- webpack.dev.js
+++ webpack.dev.js
@@ -1,7 +1,6 @@
 
 const merge = require('webpack-merge');
 const common = require('./webpack.common');
-const webpack = require('webpack');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 
@@ -12,11 +11,14 @@ module.exports = merge(common, {
         preLoader: './src/preLoader.js'
     },
     devtool: 'inline-source-map',
+    devServer: {
+        static: './dist',
+        historyApiFallback: true
+    },
     plugins: [
         new CleanWebpackPlugin(),
         new CopyWebpackPlugin([
             { from: 'public' }
-        ]),
-        new webpack.HotModuleReplacementPlugin(),
+        ])
     ]
 });
