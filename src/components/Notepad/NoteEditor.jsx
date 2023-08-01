2357ee70ce4101fa566dc523d89e16ca8770079e
Konrad C <conrisc@gmail.com>
2023-08-01 21:51:54 +0200

    [Build] Add icon indicating that note has been saved

--- src/components/Notepad/NoteEditor.jsx
+++ src/components/Notepad/NoteEditor.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useMemo, useState } from 'react';
 import { throttle } from 'throttle-debounce';
 import { NoteItem } from 'mellov_api';
 import { Input } from 'antd';
@@ -6,37 +6,58 @@ import { getUsersApi } from 'Services/mellowApi';
 
 const { TextArea } = Input;
 
-async function saveNote(noteId, text) {
-	const noteItem = new NoteItem();
-	noteItem.id = noteId;
-	noteItem.creationDate = new Date().toISOString();
-	noteItem.text = text;
-
-	const api = await getUsersApi();
-	api.updateNote(noteId, noteItem).then(
-		() => {
-			console.log('Note updated');
-		},
-		(error) => {
-			console.warn('Error while saving note: ', error);
-		}
-	);
-}
-
-const handleNoteUpdate = throttle(3000, saveNote);
 
 export function NoteEditor(props) {
 	const noteId = props.note.id;
 	const [text, setText] = useState(props.note.text);
+	const [isSaved, setIsSaved] = useState(true);
 	const textRef = React.createRef();
+
+	const handleNoteUpdate = useMemo(() => {
+		return throttle(3000, async (noteId, text) => {
+			await saveNote(noteId, text);
+			setIsSaved(true);
+		});
+	}, [])
 
 	useEffect(() => {
 		setText(props.note.text);
 		textRef.current.focus();
 	}, [noteId]);
 
+
+	async function saveNote(noteId, text) {
+		const noteItem = new NoteItem();
+		noteItem.id = noteId;
+		noteItem.creationDate = new Date().toISOString();
+		noteItem.text = text;
+
+		const api = await getUsersApi();
+		api.updateNote(noteId, noteItem).then(
+			() => {
+				console.log('Note updated');
+			},
+			(error) => {
+				console.warn('Error while saving note: ', error);
+			}
+		);
+	}
+
 	return (
-		<div>
+		<div style={{ position: 'relative' }}>
+			{isSaved && (
+				<div
+					style={{
+						position: 'absolute',
+						top: 4,
+						right: 8,
+						zIndex: 10,
+						color: '#52c41a',
+					}}
+				>
+					<i className="fas fa-check"></i>
+				</div>
+			)}
 			<TextArea
 				ref={textRef}
 				autoFocus
@@ -45,6 +66,7 @@ export function NoteEditor(props) {
 				onChange={(e) => {
 					const t = e.target.value;
 					setText(t);
+					setIsSaved(false);
 					props.onNoteChange(noteId, t);
 					handleNoteUpdate(noteId, t);
 				}}
